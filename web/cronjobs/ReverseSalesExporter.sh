34f1ef9de36e428a18ddb477ebea34eb3b0f257f
Larry Yu <yuxiaofengch@hotmail.com>
2016-06-29 15:42:32 +1000

    MAG-61: fix bugs

--- web/cronjobs/ReverseSalesExporter.sh
+++ web/cronjobs/ReverseSalesExporter.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-## run reverse xero exporter ########################################
-if ps ax | grep -v grep | grep "ReverseSalesReportRunnerphp" > /dev/null; then
-echo -n "ReverseSalesReportRunner is Already Running....... :: "
-date
-echo -n " "
+TODAY=`date +%d`
+TOMORROW=`date +%d -d "1 day"`
+
+# See if tomorrow's day is less than today's
+if [ $TOMORROW -lt $TODAY ]; then
+	echo "This is the last day of the month"
+	## run reverse xero exporter ########################################
+	if ps ax | grep -v grep | grep "ReverseSalesReportRunnerphp" > /dev/null; then
+	echo -n "ReverseSalesReportRunner is Already Running....... :: "
+	date
+	echo -n " "
+	else
+	/usr/bin/php /var/www/magentob2b/web/cronjobs/report/ReverseSalesReportRunner.php >> /tmp/log/ReverseSalesReportRunner_`date +"%d_%b_%y"`.log
+	fi
 else
-/usr/bin/php /var/www/magentob2b/web/cronjobs/report/ReverseSalesReportRunner.php >> /tmp/log/ReverseSalesReportRunner_`date +"%d_%b_%y"`.log
+	echo -n "This is not the last day of the month::"
+	date
+	exit
 fi
-
-
