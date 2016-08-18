0b192db8046163681e90c8eb6355c4450eb61204
Larry Yu <yuxiaofengch@hotmail.com>
2016-08-18 16:00:01 +1000

    MAG22: fix bug

--- web/protected/pages/PurchaseOrder/Details/DetailsController.php
+++ web/protected/pages/PurchaseOrder/Details/DetailsController.php
@@ -47,9 +47,9 @@ class DetailsController extends DetailsPageAbstract
 		$statusOptions =  $purchaseOrder->getStatusOptions();
 		$purchaseOrderItems = array();
 		foreach (PurchaseOrderItem::getAllByCriteria('purchaseOrderId = ?', array($purchaseOrder->getId()), true, null, DaoQuery::DEFAUTL_PAGE_SIZE, array('updated'=>'desc')) as $item) {
-			$product = Product::get($item->getProduct()->getId());
+			$product = $item->getProduct();
 			if(!$product instanceof Product)
-				throw new Exception('Invalid Product passed in!');
+				continue;
 			$unitPrice = $item->getUnitPrice();
 			$qty = $item->getQty();
 			$totalPrice = $item->getTotalPrice();
