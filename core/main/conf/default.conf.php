7e3648c292ae2952c8a17607804f2fb8a9e79968
Philip Xiao <philip.f.xiao@gmail.com>
2017-02-28 18:37:43 +1100

    MAG-142:Redesign the product info panel update

--- core/main/conf/default.conf.php
+++ core/main/conf/default.conf.php
@@ -2,10 +2,10 @@
 return array(
 	'Database' => array(
 			'Driver' => 'mysql',
-			'DBHost' => 'backup5.caqb2yq2sxkg.ap-southeast-2.rds.amazonaws.com:3306',
+			'DBHost' => 'localhost',
 			'DB' => 'bpcinternal_multistore',
-			'Username' => 'budgetpc',
-			'Password' => 'budget123pc'
+			'Username' => 'root',
+			'Password' => 'root'
 		)
 );
 
