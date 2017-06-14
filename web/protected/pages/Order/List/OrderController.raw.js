a0a56498314b0cddd6d5fbd0ca89448b8ad9c03c
Philip Xiao <philip.f.xiao@gmail.com>
2017-06-14 13:25:05 +1000

    MAG-153: order list and creditnote adjustment

--- web/protected/pages/Order/List/OrderController.raw.js
+++ web/protected/pages/Order/List/OrderController.raw.js
@@ -121,7 +121,7 @@ PageJs.prototype = Object.extend(new BPCPageJs(), {
 					$(tmp.me.totalDueId)
 					.update(
 						new Element('span', {'title': 'Total Amt: ' + tmp.me.getCurrency(tmp.result.totalAmount) + '\nTotal Paid: ' + tmp.me.getCurrency(tmp.result.totalPaid) + '\nTotal Credited: ' + tmp.me.getCurrency(tmp.result.totalCreditNoteValue) + '\nTotal PaidViaCrdit: ' + tmp.me.getCurrency(tmp.result.paidViaCredit) }).update(
-							tmp.me.getCurrency(tmp.result.totalAmount - tmp.result.totalPaid - tmp.result.totalCreditNoteValue + (tmp.result.paidViaCredit * 1))
+							tmp.me.getCurrency(tmp.result.totalAmount - tmp.result.totalPaid - tmp.result.totalCreditNoteValue + (tmp.result.paidViaCredit * 1) + (tmp.result.totalRefund * 1))
 						)
 					);
 					
